5cb80aabb7ced480b93cb18bc9a367d0a4f5e69f
raduubraduu <lefter_radu@yahoo.com>
2023-04-09 22:31:18 +0100

    added fallback for when no expenses

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -5,7 +5,7 @@ import { ExpensesContext } from "../store/expenses-context";
 function AllExpenses() {
   const expensesCtx = useContext(ExpensesContext);
 
-  return <ExpensesOutput expenses={expensesCtx.expenses} expensesPeriod="Total" />;
+  return <ExpensesOutput expenses={expensesCtx.expenses} expensesPeriod="Total" fallbackText="No registered expenses found!"/>;
 }
 
 export default AllExpenses;

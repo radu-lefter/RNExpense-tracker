b443c7d329618c9abbc3cc664791161dd4cba39b
raduubraduu <lefter_radu@yahoo.com>
2023-04-10 18:43:47 +0100

    added error handling

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,27 +1,39 @@
-import { useContext, useEffect, useState  } from "react";
+import { useContext, useEffect, useState } from "react";
 import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/expenses-context";
 import { getDateMinusDays } from "../util/date";
 import { fetchExpenses } from "../util/http";
-import LoadingOverlay from '../components/UI/LoadingOverlay';
+import LoadingOverlay from "../components/UI/LoadingOverlay";
+import ErrorOverlay from '../components/UI/ErrorOverlay';
 
 function RecentExpenses() {
   const [isFetching, setIsFetching] = useState(true);
+  const [error, setError] = useState();
+
   const expensesCtx = useContext(ExpensesContext);
 
   useEffect(() => {
     async function getExpenses() {
       setIsFetching(true);
-      const expenses = await fetchExpenses();
+      try {
+        const expenses = await fetchExpenses();
+
+        expensesCtx.setExpenses(expenses);
+      } catch (error) {
+        setError("Could not fetch expenses!");
+      }
       setIsFetching(false);
-      expensesCtx.setExpenses(expenses);
     }
 
     getExpenses();
   }, []);
 
+  if (error && !isFetching) {
+    return <ErrorOverlay message={error} />;
+  }
+
   if (isFetching) {
-    return <LoadingOverlay />
+    return <LoadingOverlay />;
   }
 
   const recentExpenses = expensesCtx.expenses.filter((expense) => {
